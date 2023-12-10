1956ce1cc608769f591899f15314c998b92b9f6f
Edward Fazackerley <25fazedw@cbc.sa.edu.au>
2023-12-10 18:36:59 +1030

    Update SoundProof description and argument

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import wave
 
 parser = argparse.ArgumentParser(
                     prog='SoundProof',
-                    description='An audio encoder and decoder for secure communication????',
+                    description='An audio encoder and decoder (for hiding messages in audio files).',
                     epilog='Made by CloudWaddie')
-parser.add_argument('--encode', help='Encode a file', metavar='FILENAME')
-parser.add_argument('--decode', help='Decode a file', metavar='FILENAME')
+parser.add_argument('-t', help='Encode/Decode a file', metavar='ENCODE/DECODE')
+## Not needed: parser.add_argument('--decode', help='Decode a file', metavar='FILENAME')
 args = parser.parse_args()
 try: print(args.filename)
 except: print('No file specified\nRun with --help for more info')
