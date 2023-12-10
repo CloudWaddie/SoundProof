eb55b70e08258b639614681e10d0fad2a12d5a88
Edward Fazackerley <25fazedw@cbc.sa.edu.au>
2023-12-10 18:34:16 +1030

    Add encode and decode options to argparse

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ parser = argparse.ArgumentParser(
                     prog='SoundProof',
                     description='An audio encoder and decoder for secure communication????',
                     epilog='Made by CloudWaddie')
-parser.add_argument('filename')  # Input file
+parser.add_argument('--encode', help='Encode a file', metavar='FILENAME')
+parser.add_argument('--decode', help='Decode a file', metavar='FILENAME')
 args = parser.parse_args()
-print(args.filename)
+try: print(args.filename)
+except: print('No file specified\nRun with --help for more info')
