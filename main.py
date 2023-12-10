8c16312d892646425dc50718c8a2308105b19797
Edward Fazackerley <25fazedw@cbc.sa.edu.au>
2023-12-10 18:48:52 +1030

    Add required flag to -t option and handle decoding in main.py

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ parser = argparse.ArgumentParser(
                     prog='SoundProof',
                     description='An audio encoder and decoder (for hiding messages in audio files).',
                     epilog='Made by CloudWaddie')
-parser.add_argument('-t', help='Encode/Decode a file', metavar='ENCODE/DECODE')
+parser.add_argument('-t', help='Encode/Decode a file', metavar='ENCODE/DECODE', required=True)
+parser.add_argument('filename', help='File to encode/decode', metavar='FILENAME')
 ## Not needed: parser.add_argument('--decode', help='Decode a file', metavar='FILENAME')
 args = parser.parse_args()
 try:
     print(args.filename)
-except:
+    if args.t.lower() == 'decode':
+        print('Decoding...')
+        loadfile = wave.open(args.filename, 'rb')
+except argparse.ArgumentError:
+    print()
     print('No file specified\nRun with --help for more info')
     exit("Exited. Reason: No file specified")
