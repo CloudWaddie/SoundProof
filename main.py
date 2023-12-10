4bf22e6545c04e13e1d17a29e57ac00e53b6ad6a
Edward Fazackerley <25fazedw@cbc.sa.edu.au>
2023-12-10 18:51:06 +1030

    Add initialization and decoding functions

--- main.py
+++ main.py
@@ -2,19 +2,27 @@
 import argparse
 import wave
 
+def init():
+    print('SoundProof v1.0')
+    print('Made by CloudWaddie')
+    print('==================')
+def decode():
+        print(args.filename)
+        if args.t.lower() == 'decode':
+            print('Decoding...')
+            loadfile = wave.open(args.filename, 'rb')
+init()
+
 parser = argparse.ArgumentParser(
                     prog='SoundProof',
                     description='An audio encoder and decoder (for hiding messages in audio files).',
                     epilog='Made by CloudWaddie')
 parser.add_argument('-t', help='Encode/Decode a file', metavar='ENCODE/DECODE', required=True)
 parser.add_argument('filename', help='File to encode/decode', metavar='FILENAME')
-## Not needed: parser.add_argument('--decode', help='Decode a file', metavar='FILENAME')
+# Not needed: parser.add_argument('--decode', help='Decode a file', metavar='FILENAME')
 args = parser.parse_args()
 try:
-    print(args.filename)
-    if args.t.lower() == 'decode':
-        print('Decoding...')
-        loadfile = wave.open(args.filename, 'rb')
+    decode()
 except argparse.ArgumentError:
     print()
     print('No file specified\nRun with --help for more info')
