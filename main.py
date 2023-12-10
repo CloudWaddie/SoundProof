78f3d343fd97e3fb3aa1c6f6e9cb21616899b29c
Edward Fazackerley <25fazedw@cbc.sa.edu.au>
2023-12-10 18:38:20 +1030

    Refactor file input handling in main.py

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-t', help='Encode/Decode a file', metavar='ENCODE/DECODE')
 ## Not needed: parser.add_argument('--decode', help='Decode a file', metavar='FILENAME')
 args = parser.parse_args()
-try: print(args.filename)
-except: print('No file specified\nRun with --help for more info')
+try:
+    print(args.filename)
+except:
+    print('No file specified\nRun with --help for more info')
+    exit("Exited. Reason: No file specified")
