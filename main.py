8f8569a11164ea7c635b17fed246d60b98fceac4
Edward Fazackerley <25fazedw@cbc.sa.edu.au>
2023-12-10 18:28:04 +1030

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Import dependencies - will be packaged in the executable
+import argparse
+import wave
+
+parser = argparse.ArgumentParser(
+                    prog='SoundProof',
+                    description='An audio encoder and decoder for secure communication????',
+                    epilog='Made by CloudWaddie')
